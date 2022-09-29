5fe0d75f484884c7a798b8829c1f285a5a566f7c
Carlos Lobera <carlosclobera@gmail.com>
2022-09-29 17:40:49 -0500

    o3l3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -1,12 +1,13 @@
 public class Objective3Lab3 {
   public static void main(String[] args) {
-    int first, second, third, sum, equation;
+    int first, second, third, sum;
 
     first = 1 + (int)(Math.random() * 6);
     second = 1 + (int)(Math.random() * 6);
     third = 1 + (int)(Math.random() * 6);
     sum = first + second + third;
-    equation = first + " + " + second + " + " + third + " +" + " = " + sum;
+
+    String equation = first + " + " + second + " + " + third + " +" + " = " + sum;
 
     System.out.println(equation);
 
