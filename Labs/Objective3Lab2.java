618ec397e6e78d21f2f2c3b4d6ebe17ce478ecdb
Carlos Lobera <carlosclobera@gmail.com>
2022-09-29 16:45:46 -0500

    o3l2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -1,10 +1,12 @@
 public class Objective3Lab2 {
   public static void main(String[] args) {
-    Double side1, side2, hypotenuse;
+    Double side1, side2, side1sqr, side2sqr, hypotenuse;
 
     side1 = 8;
     side2 = 10;
-    hypotenuse = Math.sqrt(side1*side1 + side2*side2);
+    side1sqr = side1 * side1;
+    side2sqr = side2 * side2;
+    hypotenuse = Math.sqrt(side1sqr + side2sqr);
 
     System.out.Println(hypotenuse);
   }
