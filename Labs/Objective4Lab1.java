0881db1b2ad3ed0a65b7b8c0897ef18c30414f66
Carlos Lobera <carlosclobera@gmail.com>
2022-09-29 17:54:29 -0500

    o4l1

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,11 @@
+1 public class ScannerExample {
+2
+3  public static void main(String[] args) {
+4
+5     java.util.Scanner scanner = new java.util.Scanner(System.in);
+6     System.out.println("Please type something in!");
+7     String userInput = keyboard.nextLine();
+8     System.out.println("Hello " + userInput + "!");
+12
+13   }
+14 }
