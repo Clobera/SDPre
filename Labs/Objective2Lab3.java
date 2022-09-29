de5825395ae99224ad8f6c078dc0b4e915d603c9
Carlos Lobera <carlosclobera@gmail.com>
2022-09-29 14:24:42 -0500

    o2l3

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -1,7 +1,12 @@
 public class Objective2Lab3 {
   public static void main(String[] args) {
-    int 15 = 1996;
-    int 6 = "June";
-    int 2 = false ;
+    int 15;
+    15 = 1996
+    int 6;
+    6 = "June"
+    int 2;
+    2 = false
+
+
   }
 }
