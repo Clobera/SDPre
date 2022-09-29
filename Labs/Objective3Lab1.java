4868538cf1f525c9b063f0118efba38a1ad00dd5
Carlos Lobera <carlosclobera@gmail.com>
2022-09-29 16:01:44 -0500

    o3l1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    int num1 = 15;
+    int num2 = 6;
+    int num3 = 2;
+    int average = (num1 + num2 + num3)%3;
+
+    System.out.println(average);
+  }
+}
