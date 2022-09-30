ed8dc77a967b5305fcc6d3cc4acef200ad415837
Carlos Lobera <carlosclobera@gmail.com>
2022-09-29 19:37:55 -0500

    o4l1

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Objective4Lab1 {
   public static void main(String[] args) {
 
-    Scanner keyboard = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Enter First Name" + fname);
     String fname = scanner.nextline();
