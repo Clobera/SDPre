48f22c3235936a1e8d95f64de6cb9d927abac07c
Carlos Lobera <carlosclobera@gmail.com>
2022-09-29 21:38:16 -0500

    Objective 4

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    int birthYear, age, currentYear;
+
+    currentYear = 2022;
+    birthYear = current currentYear - age;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    age = input.nextInt();
+
+    System.out.println("You were born in " + birthYear);
+    input.close();
+  }
+}
