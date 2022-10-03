d9a31f2fe6323191a130ea731ea5244572166a72
Carlos Lobera <carlosclobera@gmail.com>
2022-10-03 15:06:19 -0500

    Objective 5

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,28 @@
+public class Objective5Lab3 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int userNum;
+
+      System.out.println("Please enter a number: ");
+      userNum = scanner.nextInt();
+
+      System.out.println(userNum);
+
+      if(userNum.equals(0)) {
+        System.out.println("The number you entered is equal to zero.");
+
+      }
+
+      else if(userNum > 0) {
+        System.out.println("The number you entered is greater than zero.");
+
+      }
+
+      else if(userNum < 0) {
+        System.out.println("The number you entered is less than zero.");
+      }
+
+
+      scanner.close();
+  }
+}
