f6b4288fae867fd92c76d29f5316849be021e716
Carlos Lobera <carlosclobera@gmail.com>
2022-10-04 12:25:10 -0500

    Objective 7

--- Labs/Objective7Lab5.java
+++ Labs/Objective7Lab5.java
@@ -4,6 +4,7 @@ public class Objective7Lab5 {
     Scanner scanner = new Scanner(System.in);
     int selection;
 
+
     while (true) {
       System.out.println("_____Menu_____");
       System.out.println("1: Say Hello");
@@ -13,19 +14,19 @@ public class Objective7Lab5 {
 
       selection = scanner.nextInt();
 
-      switch(selection){
-        case 1:
-          System.out.println("Hello Human");
-          break;
-        case 2:
-          System.out.println("Tacos, Burritos, Chips & Queso");
-          break;
-        case 3:
-          System.out.println("Goodbye!");
-          break;
-        default:
-          System.out.println("Invalid entry: Please choose from the menu.");
-        }
+      if(selection == 1){
+        System.out.println("Hello Human");
+      }
+      else if(selection == 2){
+        System.out.println("Tacos, Burritos, Chips & Queso");
+      }
+      else if(selection == 3) {
+        System.out.println("Goodbye!");
+        break;
+      }
+      else {
+        System.out.println("Invalid entry: Please choose from the menu.");
+      }
 
 
     }
