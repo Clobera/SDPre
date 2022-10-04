bcb4aae5db0e5716872ce63acf0477c8ef4fea08
Carlos Lobera <carlosclobera@gmail.com>
2022-10-04 11:12:34 -0500

    Objective 7

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,16 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int currentsum, counter;
+    counter = 1
+    currentsum = 0
+
+    while(counter <= 20) {
+      currentsum += counter;
+      counter = counter + 1;
+    }
+
+    System.out.println(currentsum);
+
+    
+  }
+}
