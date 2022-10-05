c0faa2916bed1a8041e9b0a6d19ff601ae616a8e
Carlos Lobera <carlosclobera@gmail.com>
2022-10-05 10:25:14 -0500

    Objective 8

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,14 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int i = 0; i <=10; i++) {
+      System.out.println(i);
+    }
+
+    for (int e = 10; e <= 10; e -- 1) {
+      System.out.println(e);
+    }
+
+
+  }
+}
