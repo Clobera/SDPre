8d46adb63abad870d606b68069297ffca5d80fdb
Carlos Lobera <carlosclobera@gmail.com>
2022-10-05 11:08:15 -0500

    Objective 8

--- Labs/Objective8Lab2.java
+++ Labs/Objective8Lab2.java
@@ -1,7 +1,7 @@
 public class Objective8Lab2{
   public static void main(String[] args) {
     int currentsum;
-    currentsum = 0
+    currentsum = 0;
 
     for (int i = 0; i <= 20; i++) {
       System.out.println(i);
