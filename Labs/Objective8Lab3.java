9dbcddba92dd857e6a29ac87c2d79c96c51b9765
Carlos Lobera <carlosclobera@gmail.com>
2022-10-06 12:15:04 -0500

    Objective 8

--- Labs/Objective8Lab3.java
+++ Labs/Objective8Lab3.java
@@ -1,8 +1,7 @@
 public class Objective8Lab3 {
   public static void main(String[] args) {
-    int counter = 1;
 
-    for (counter; counter <= 20; counter++) {
+    for (int counter = 1; counter <= 20; counter++) {
     }
 
     if(counter % 2 == 0){
